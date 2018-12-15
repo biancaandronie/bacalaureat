8c09c2d39746851d0100c28e0a44e67a3fa1d95a
Bianca <andronie27@gmail.com>
2018-12-15 17:10:17 +0200

    added menu

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ class App extends Component {
           <img src={imag} className="App-logo" alt="crop1" width="1600" height="158" />
           <img src={foot} className="footer" alt="crop1" width="1600" height="154" />
 
+          <div className="menu">
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#news">News</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="#about">About</a></li>
+            </ul>
+          </div>
+
       </div>
 
 
