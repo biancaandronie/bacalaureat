c7614e8cc71a59c2d93887d48d2497730257d045
Bianca <andronie27@gmail.com>
2018-12-15 15:11:53 +0200

    [2 com]

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ class App extends Component {
       <div className="App">
           <img src={imag} className="App-logo" alt="crop1" width="1600" height="158" />
 
-        <div align="center" style="width:100%; height:auto;">
-        <table width="100%" border="0" cellspacing="0" cellpadding="0" className="App-logo">
-        <tr>
-        <td>
-         <img src={imag} alt="Girl in a jacket" />
-        </td>
-        <td>
-         <h1>aaa</h1>
-        </td>
-        </tr>
-        </table>
-        </div>
+      </div>
 
     );
   }
