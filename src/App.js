536b07c2d2bc4c53ba06d40951b924cc86438299
Bianca <andronie27@gmail.com>
2018-12-15 14:21:22 +0200

    [add className jumatate]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import imag from './images/crop1.jpg';
+import stg from './images/stg.jpg';
+import dr from './images/dr.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
           <img src={imag} className="App-logo" alt="crop1" width="1600" height="158" />
 
+          <div className="jumatate">
+
+            <div className="stanga">
+                <img src={stg} className="App-logo" alt="crop1" width="60" height="888" />
+            </div>
+
+            <div className="centru">
+                <h>alb</h>
+            </div>
+
+            <div className="dreapta">
+                <img src={dr} className="App-logo" alt="crop1" width="60" height="888" />
+            </div>
+          </div>
       </div>
     );
   }
