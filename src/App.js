c8c37f1b7fbd2928a47fc157e02a543a58073520
Bianca <andronie27@gmail.com>
2018-12-16 22:12:54 +0200

    carusel-math

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ class App extends Component {
                    <ol className="carousel-indicators">
                      <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                     <li data-target="#myCarousel" data-slide-to="2"></li>
+                     <li data-target="#myCarousel" data-slide-to="3"></li>
                    </ol>
 
 
