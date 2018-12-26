60710344a405ba0146a89410e41f2d45f3ced1e4
Bianca <andronie27@gmail.com>
2018-12-26 19:32:04 +0200

    [2 com]

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ class App extends Component {
 
             <div class="pt5 ph3-ns pv4-ns pv2 center flex flex-column items-center justify-center w-100 css-1cvvhpb">
                 <div class="css-q5fqw0">
-                    <img class="mt4-l mt3 css-1kd9lzd" src={trompa1} >
+                    <img class="mt4-l mt3 css-1kd9lzd" src={trompa1} />
                 </div>
 
                 <section class="css-rlvjp6">
