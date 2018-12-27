9eef9c9e4a81c7cd3ec2a9c5b45064b6fd1d3288
Bianca <andronie27@gmail.com>
2018-12-27 14:04:07 +0200

    [2 com]

--- src/App.js
+++ src/App.js
@@ -108,14 +108,18 @@ class App extends Component {
             </div>
         </div>
 
-            <div className="pt5 ph3-ns pv4-ns pv2 center flex flex-column items-center justify-center w-100 css-1cvvhpb">
-                <div className="css-q5fqw0">
-                    <img className="mt4-l mt3 css-1kd9lzd" src={trompa1} width="402" height="297" />
-                </div>
-
-
-
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6">
+              <h1>Bootstrap Grid Demo</h1>
             </div>
+            <div class="col-sm-6 other">
+              <h1>2 Columns</h1>
+            </div>
+          </div>
+        </div>
+
+
       </div>
 
 
