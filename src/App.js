dee4004680e82663905483bb1ac362fe807ef301
Bianca <andronie27@gmail.com>
2018-12-27 17:34:00 +0200

    [2 com]

--- src/App.js
+++ src/App.js
@@ -115,10 +115,9 @@ class App extends Component {
 
                 <div className="w3-row-padding">
                   <div className="w3-third w3-container w3-margin-bottom">
-                    <a className="sans-serif grow css-uwcdxx" href="#home" aria-label="">
-                    <img src={bac5} className="css-19ui7ac" /><div></div>
-
-                    </a>
+                    <div className="w3-third w3-container w3-margin-bottom">
+                       <img src={bac5} alt="Norway" className="w3-hover-opacity" />
+                    </div>
 
                   </div>
                   <div className="w3-third w3-container w3-margin-bottom">
