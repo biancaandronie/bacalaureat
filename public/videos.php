f7895cd0fdc2e559ee79d5a1fc48d2775cde3490
bianca <bianca@bianca.local>
2018-12-28 17:26:13 +0200

    adding two dimensions array

--- public/videos.php
+++ public/videos.php
@@ -2,14 +2,17 @@
 $domain = 'http://bacalaureat.local/';
 $it = new RecursiveDirectoryIterator("videos");
 $list_videos = array();
-foreach(new RecursiveIteratorIterator($it) as $file) {
-    $FILE = array_flip(explode('.', $file));
-    if (isset($FILE['php']) || isset($FILE['mp4'])) {
-	$link = $domain.$file;
-	$video = substr($file,7,-4);
-	$list_videos[$video] = $link;
+  for ($i = 1; $i <= 10; $i++) {
+	$list_videos = array();
+	foreach(new RecursiveIteratorIterator($it) as $file) {
+    	$FILE = array_flip(explode('.', $file));
+    		if (isset($FILE['php']) || isset($FILE['mp4'])) {
+			$link = $domain.$file;
+			$video = substr($file,7,-4);
+			$list_videos[] = array("name" => "$video","link" => "$link");
 	  
-    }
+    		}
+  	}
 }
 echo json_encode($list_videos);
 ?>
