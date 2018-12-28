7fb27a864a19e4fc0f1907f2a4e56819a7e3ccdd
Bianca <andronie27@gmail.com>
2018-12-28 17:37:37 +0200

    [2 com]

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Suggestions = (props) => {
   const options = props.results.map(r => (
-    <li key={r.link}>
-      {r.name}
+    <li key={r.name}>
+       {r.link}
+
     </li>
   ))
   return <ul>{options}</ul>
