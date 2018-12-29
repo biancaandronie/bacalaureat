444279179315edaf4f4c20bf8a05dd762bbed883
Bianca <andronie27@gmail.com>
2018-12-29 19:10:34 +0200

    [2 com]

--- src/App.js
+++ src/App.js
@@ -222,42 +222,7 @@ class App extends Component {
             width={300}
             height={40}
           >
-            {({
-              getInputProps,
-              getItemProps,
-              getLabelProps,
-              isOpen,
-              inputValue,
-              highlightedIndex,
-              selectedItem,
-            }) => (
-              <div>
-                <label {...getLabelProps()}>Enter a fruit</label>
-                <input {...getInputProps()} />
-                {isOpen ? (
-                  <div>
-                    {data
-                      .filter(item => !inputValue || item.name.includes(inputValue))
-                      .map((item, index) => (
-                        <div
-                          {...getItemProps({
-                            key: item.name,
-                            index,
-                            item,
-                            style: {
-                              backgroundColor:
-                                highlightedIndex === index ? 'lightgray' : 'white',
-                              fontWeight: selectedItem === item ? 'bold' : 'normal',
-                            },
-                          })}
-                        >
-                          {item.name}
-                        </div>
-                      ))}
-                  </div>
-                ) : null}
-              </div>
-            )}
+
           </Downshift>
 
 
