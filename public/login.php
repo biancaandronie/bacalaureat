52a35e9e19315f9dc1f081473570180dd9954912
Bianca <andronie27@gmail.com>
2019-02-18 21:09:16 +0200

    something

--- public/login.php
+++ public/login.php
@@ -1,5 +1,32 @@
 
 <?php
+header("Access-Control-Allow-Origin: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+if( empty($_POST['fname']) && empty($_POST['parola']) ) die();
+
+if ($_POST){
+    // set response code - 200 OK
+    http_response_code(200);
+    $username = $_POST['fname'];
+    $parola = $_POST['parola']
+
+    //Headers
+    $headers  = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+    //echo json_encode( $_POST );
+    echo json_encode(array("sent" => true));
+} else {
+ // tell the user about error
+ echo json_encode(
+     [
+        "sent" => false,
+        "message" => "Something went wrong"
+     ]
+ );
+}
 
 function conectare($host = "localhost", $username = "root", $password = "", $dbname = "LOGIN") {
 
@@ -44,7 +71,7 @@ function login($u, $p) {
     }
 }
 
-
+login($username,$parola)
 
 
 
