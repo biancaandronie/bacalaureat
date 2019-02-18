7800b7452ef5668d8ed37d2eb6b52beb6025e90b
Bianca <andronie27@gmail.com>
2019-02-18 20:09:59 +0200

    [2 com]

--- src/Login.js
+++ src/Login.js
@@ -18,18 +18,18 @@ constructor(props) {
                 <h1 className='bienvenue'>{this.props.text}</h1>
                     <div>
                         <form action="#">
-                            <input className='deep-blue-gradient' type="text" id="fname" name="firstname" placeholder="Nom d'utilisateur"
+                            <input className='winter-neva-gradient' type="text" id="fname" name="firstname" placeholder="Nom d'utilisateur"
                                    value={this.state.fname }
                                    onChange={e => this.setState({ fname: e.target.value })}
                             /> <br />
-                            <input className='deep-blue-gradient' type="text" id="parola" name="password" placeholder="Mot de passe"
+                            <input className='winter-neva-gradient' type="text" id="parola" name="password" placeholder="Mot de passe"
                                    value={this.state.parola }
                                    onChange={e => this.setState({ parola: e.target.value })}
                             />
                         </form>
                     </div>
-        <button id='log' className='btn deep-blue-gradient rounded-circle'>Login</button>
-        <button id='exit' className='btn deep-blue-gradient rounded-circle' onClick={this.props.closePopup}>sortie</button>
+        <button id='log' className='btn winter-neva-gradient rounded-circle'>Login</button>
+        <button id='exit' className='btn winter-neva-gradient rounded-circle' onClick={this.props.closePopup}>sortie</button>
         </div>
       </div>
     );
