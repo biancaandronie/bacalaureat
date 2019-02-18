a1a720c9dc359825e96d9933b844efad41e29a3d
Bianca <andronie27@gmail.com>
2019-02-18 21:14:08 +0200

    [2 com]

--- public/login.php
+++ public/login.php
@@ -1,7 +1,7 @@
 
 <?php
 header("Access-Control-Allow-Origin: *");
-$rest_json = file_get_contents("php://button");
+$rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
 if( empty($_POST['fname']) && empty($_POST['parola']) ) die();
