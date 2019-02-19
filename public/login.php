7840de1a6c54f329241ee1bb6de23dd3cafd5222
Bianca <andronie27@gmail.com>
2019-02-19 11:33:34 +0200

    wdasda

--- public/login.php
+++ public/login.php
@@ -51,7 +51,7 @@ function login($u, $p) {
     }
 }
 header("Access-Control-Allow-Origin: *");
-$rest_json = file_get_contents("php://button");
+$rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
 if( empty($_POST['fname']) && empty($_POST['parola']) ) die();
