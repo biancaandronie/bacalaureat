dacbea87a8f028d03e589ca8a75d67a40303ff3c
Bianca <andronie27@gmail.com>
2019-02-19 13:40:08 +0200

    wdasda

--- src/Login.js
+++ src/Login.js
@@ -69,16 +69,13 @@ validateForm() {
 
                             <button type="submit" id='log' className='btn winter-neva-gradient rounded-circle' onClick = {this.loginHandle.bind(this)} value="Login">Login</button>
 
-//                            <Route render={({match})=>(
-//                                this.state.loggedIn ? ( <Redirect to='https://www.youtube.com/' />) : (<Redirect to='/aaa' />)
-//                            )}/>
+                            <Route path='' render={({match})=>(
+                                this.state.loggedIn ? ( <Redirect to='https://www.youtube.com/' />) : (<Redirect to='/aaa' />)
+                            )}/>
 
                             <div>
                                 {this.state.mailSent  &&
-                                  <Route render={({match})=>(
-                                      this.state.loggedIn ? ( <Redirect to='https://www.youtube.com/' />) : (<Redirect to='/aaa' />)
-                                  )}/>
-                                  //<div className="sucsess">Thank you for contacting me.</div>
+                                  <div className="sucsess">Thank you for contacting me.</div>
                                 }
                                 {this.state.error  &&
                                   <div className="error">Sorry we had some problems.</div>
