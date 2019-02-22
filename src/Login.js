b3fc63397d7b19271c7649a178dc623dab5de24f
Bianca <andronie27@gmail.com>
2019-02-22 22:04:17 +0200

    wdasda

--- src/Login.js
+++ src/Login.js
@@ -11,8 +11,8 @@ class Popup extends Component {
 constructor(props) {
     super(props);
     this.state = {
-      username: 'bianca',
-      password: 'parola',
+      username: '',
+      password: '',
       redirectToReferrer: false
     }
     this.login = this.login.bind(this);
