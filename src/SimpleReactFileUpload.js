e894bdac0401c8100305de546a7cb8e9e4d07a4f
Bianca <andronie27@gmail.com>
2019-04-06 16:43:42 +0300

    upload12

--- src/SimpleReactFileUpload.js
+++ src/SimpleReactFileUpload.js
@@ -25,13 +25,13 @@ import Dropzone from 'react-dropzone-uploader';
         />
         <div class="container">
           <form>
-            <div class="form-group">
-              <label for="usr">Name:</label>
-              <input type="text" class="form-control" id="usr">
+            <div className="form-group">
+              <label htmlFor="usr">Name:</label>
+              <input type="text" className="form-control" id="usr" />
             </div>
             <div class="form-group">
-              <label for="pwd">Password:</label>
-              <input type="password" class="form-control" id="pwd">
+              <label htmlFor="pwd">Password:</label>
+              <input type="password" className="form-control" id="pwd" />
             </div>
           </form>
         </div>
