1ac16d1834f2d049731f0be07da84ce35f7a9191
Bianca <andronie27@gmail.com>
2019-04-06 23:54:34 +0300

    up

--- src/test.js
+++ src/test.js
@@ -3,38 +3,39 @@ import React, { Component } from 'react';
 class MyForm extends Component {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.postData = this.postData.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
+   function postData(event){
+              event.preventDefault();
 
-    fetch('http://api.bacalaureat.local/api/v1/create', {
-      method: 'POST',
-      body: data,
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log('This data was successfully received by the server:');
-        console.log(data.form);
-      });
-  }
+              let name = document.getElementById('name').value;
+              let course = document.getElementById('course').value;
+              let course = document.getElementById('tag').value;
+
+              fetch('http://api.bacalaureat.local/api/v1/create', {
+                  method: 'POST',
+                  headers : new Headers(),
+                  body:JSON.stringify({name:name, course:course, tag:[tag]})
+              }).then((res) => res.json())
+              .then((data) =>  console.log(data))
+              .catch((err)=>console.log(err))
+          }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="username">Enter username</label>
-        <input id="username" name="username" type="text" />
-
-        <label htmlFor="email">Enter your email</label>
-        <input id="email" name="email" type="email" />
-
-        <label htmlFor="birthdate">Enter your birth date</label>
-        <input id="birthdate" name="birthdate" type="text" />
-
-        <button>Send data!</button>
-      </form>
+      <form  id="postData">
+              <div>
+                  <input type="text" name="" id="name">
+              </div>
+              <div>
+                  <textarea name="" id="course" cols="20" rows="5"></textarea>
+              </div>
+              <div>
+                                <textarea name="" id="tag" cols="20" rows="5"></textarea>
+              </div>
+              <input type="submit" value="SEND POST">
+          </form>
     );
   }
 }
