a3f749564dbde85e5472b5deb0f55ee1394650ac
Bianca <andronie27@gmail.com>
2019-04-06 13:59:21 +0300

    upload1

--- src/admin.js
+++ src/admin.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
   <BrowserRouter>
-    <SimpleReactFileUpload />
+    <Route path="/users" component={SimpleReactFileUpload}/>
   </BrowserRouter >
 ), document.getElementById('root_admin'));
 
