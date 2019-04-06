12d6aefc2849d58add5bab7aa78ea31697d050dd
Bianca <andronie27@gmail.com>
2019-04-06 21:56:40 +0300

    up

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -3,64 +3,27 @@ import DropToUpload from 'react-drop-to-upload';
 import {VideoData} from './service/VideoData';
 
 class Page extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-          name: '',
-          course: '',
-          tags: ''
-        }
-    this.handleDrop = this.handleDrop.bind(this);
-    this.submit = this.submit.bind(this);
-    this.onChange = this.onChange.bind(this);
+  constructor() {
+    super();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  submit() {
-    console.log("cacat");
-      if(this.state.name && this.state.course && this.state.tags){
-          VideoData('submit',this.state).then((result) => {
-          let responseJson = result;
-          console.log(responseJson);
-          if(responseJson.videoData){
-              sessionStorage.setItem('videoData',JSON.stringify(responseJson));
-              console.log("Data sent successful");
-              }
-              else
-              {
-              console.log('Data failed');
-              }
-          });
-      }
-  }
+  handleSubmit(event) {
+      event.preventDefault();
+      const data = new FormData(event.target);
 
-  onChange(e) {
-    console.log("onchange");
-    this.setState({[e.target.name]:e.target.value});
-  }
-
-  handleDrop(files) {
-    var data = new FormData();
-
-    files.forEach((file, index) => {
-      data.append('file' + index, file);
-    });
-
-    fetch('/upload', {
-      method: 'POST',
-      body: data
-    });
+      fetch('http://api.bacalaureat.local/api/v1/create', {
+        method: 'POST',
+        body: data,
+      });
   }
 
   render() {
     return (
-        <div className="container">
-              <DropToUpload
-                onDrop={ this.handleDrop }
-              >
-                Drop file here to upload
-              </DropToUpload>
+        <form onSubmit={this.handleSubmit}>
 
-                <div className="row" id="Body">
+
                  <div className="logare2">
                     <div className="form-group">
                       <label htmlFor="usr">Le nom:</label>
@@ -79,11 +42,10 @@ class Page extends Component {
                       <input type="text" name="tags" className="form-control" id="mot" onChange={this.onChange} />
                     </div>
 
-                    <input className='btn winter-neva-gradient rounded-circle' type="submit" value="Envoyer" onClick={this.submit}/>
-
+                    <button>Send data!</button>
                 </div>
-             </div>
-        </div>
+
+        </form>
     );
   }
 }
