31a880e515a5c0c0a086f39a411a23a4c06c8ec8
Bianca <andronie27@gmail.com>
2019-04-06 22:21:02 +0300

    up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import DragAndDrop from './DragAndDrop';
+import test from './test';
 import * as serviceWorker from './serviceWorker';
 //pt butonul din header--pt culoare am instalat mdbreact
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -18,6 +19,7 @@ ReactDOM.render((
     <div>
       <Route exact path="/" component={App} />
       <Route path="/admin" component={DragAndDrop} />
+      <Route path="/test" component={test} />
     </div>
   </BrowserRouter >
 ), document.getElementById('root'));
