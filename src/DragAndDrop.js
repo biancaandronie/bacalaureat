347593a849829a560454e2d1d7837ea8f32b0960
Bianca <andronie27@gmail.com>
2019-04-07 14:08:05 +0300

    try_file

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -13,24 +13,19 @@ class Page extends React.Component {
     	}
 
     onFormSubmit(e) {
-        e.preventDefault();
-        this.fileUpload(this.state.file);
-    }
+    		e.preventDefault();
+    		this.fileUpload(this.state.file);
+    	}
 
-    onChange = (e) => {
-        /*
-         Because we named the inputs to match their
-         corresponding values in state, it's
-         super easy to update the state
-         */
-        this.setState({ file: e.target.files[0] });
-    }
+    	onChange(e) {
+    		this.setState({ file: e.target.files[0] });
+    	}
 
-    fileUpload(file) {
+    	fileUpload(file) {
     		let data = new FormData();
     		data.append('data', file);
-    		const { name, course, tag, description } = this.state;
-    		axios.post('http://api.bacalaureat.local/api/v1/create', { name, course, tag, description, file }, data, {
+    		axios
+    			.post('http://api.bacalaureat.local/api/v1/create', data, {
     				headers: {
     					'Content-Type': 'multipart/form-data',
     				},
@@ -40,48 +35,14 @@ class Page extends React.Component {
     			});
     	}
 
-
-    render() {
-        const { name, course, tag, description, data, file } = this.state;
-        return (
-            <form onSubmit={this.onFormSubmit}>
-                <input type="file" name="file" onChange={this.onChange} />
-                <label htmlFor="name">Le nom:</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={this.onChange}
-                />
-                <select name="course" value={course} onChange={this.onChange}>
-                  <option selected>Choisir une matière</option>
-                  <option value="littérature">Littérature</option>
-                  <option value="informatique">Informatique</option>
-                  <option value="histoire">Histoire</option>
-                  <option value="géographie">Géographie</option>
-                  <option value="biologie">Biologie végétale et animale</option>
-                  <option value="anatomie">Anatomie et physiologie humaine</option>
-                  <option value="chimie">Chimie</option>
-                  <option value="physique">Physique</option>
-                  <option value="mathématiques">Mathématiques</option>
-                </select>
-                <label htmlFor="description">La description:</label>
-                <input
-                    type="text"
-                    name="description"
-                    value={description}
-                    onChange={this.onChange}
-                />
-                <label htmlFor="tag">Les mots-clés:</label>
-                <input
-                    type="text"
-                    name="tag"
-                    value={tag}
-                    onChange={this.onChange}
-                />
-                <button type="submit">Upload</button>
-            </form>
-        );
+    	render() {
+    		return (
+    			<form onSubmit={this.onFormSubmit}>
+    				<h1>File Upload</h1>
+    				<input type="file" onChange={this.onChange} />
+    				<button type="submit">Upload</button>
+    			</form>
+    		);
+    	}
     }
-}
 export default Page;
