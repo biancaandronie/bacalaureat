7f930575bf7fbdf1741ceddc650127bbdf01c7ab
Bianca <andronie27@gmail.com>
2019-04-07 11:15:23 +0300

    api ok

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios, { post } from 'axios';
 import DropToUpload from 'react-drop-to-upload';
 
 class Page extends Component {
