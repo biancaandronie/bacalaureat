ef349d0a5ec184ee6dd83f2cf81c2dc0cd6ee3f8
Bianca <andronie27@gmail.com>
2019-04-10 23:24:11 +0300

    change name to localhost

--- src/test.js
+++ src/test.js
@@ -28,6 +28,22 @@ class Test extends Component {
 
     }
 
+    const obj = {
+    hello: "world"
+};
+    const json = JSON.stringify(obj);
+    const blob = new Blob([json], {
+    type: 'application/json'
+});
+    const data = new FormData();
+    data.append("document", blob);
+    axios({
+              method: 'post',
+              url: '/sample',
+              data: data,
+          })
+
+
     onSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData();
@@ -38,6 +54,11 @@ class Test extends Component {
         );
         const config = { headers: { 'Access-Control-Allow-Origin': '*' } };
         const { name, course, tag, description } = this.state;
+        const json = { name, course, tag, description };
+        const blob = new Blob([json], {
+            type: 'application/json'
+        });
+        formData.append("meta", blob);
         axios.post('http://localhost:8080/api/v1/create',
             { name, course, tag, description })
             .then((result) => {
