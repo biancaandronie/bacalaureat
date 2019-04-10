29e9dd0185226a42c093733fb53056e36f9911b0
Bianca <andronie27@gmail.com>
2019-04-10 23:25:03 +0300

    change name to localhost

--- src/test.js
+++ src/test.js
@@ -28,22 +28,6 @@ class Test extends Component {
 
     }
 
-    const obj = {
-    hello: "world"
-};
-    const json = JSON.stringify(obj);
-    const blob = new Blob([json], {
-    type: 'application/json'
-});
-    const data = new FormData();
-    data.append("document", blob);
-    axios({
-              method: 'post',
-              url: '/sample',
-              data: data,
-          })
-
-
     onSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData();
