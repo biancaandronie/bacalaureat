0231f7113159b540a42d2092f6595493009c9215
Bianca <andronie27@gmail.com>
2019-04-11 20:29:49 +0300

    try3

--- src/Admin.js
+++ src/Admin.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Draggable, Droppable } from 'react-drag-and-drop';
+import './adminstyle.css';
 
 class Admin extends Component {
-    render() {
-        return (
-        <div>
-            <ul>
-                <Draggable type="fruit" data="banana"><li>Banana</li></Draggable>
-                <Draggable type="fruit" data="apple"><li>Apple</li></Draggable>
-                <Draggable type="metal" data="silver"><li>Silver</li></Draggable>
-            </ul>
-            <Droppable
-                types={['fruit']} // <= allowed drop types
-                onDrop={this.onDrop.bind(this)}>
-                <ul className="Smoothie"></ul>
-            </Droppable>
-        </div>
-        );
+    onDragStart = (event, taskName) => {
+        console.log('dragstart on div: ', taskName);
+        event.dataTransfer.setData("taskName", taskName);
     }
-    onDrop(data) {
-        console.log(data)
-        // => banana
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event, cat) => {
+        let taskName = event.dataTransfer.getData("taskName");
+
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.taskName == taskName) {
+                task.type = cat;
+            }
+            return task;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks
+        });
+    }
+    render() {
+        var tasks = {
+            inProgress: [],
+            Done: []
+        }
+
+        this.state.tasks.forEach ((task) => {
+            tasks[task.type].push(
+                <div key={task.id}
+                     onDragStart = {(event) => this.onDragStart(event, task.taskName)}
+                     draggable
+                     className="draggable"
+                     style = {{backgroundColor: task.bgcolor}}>
+                    {task.taskName}
+                </div>
+            );
+        });
+
+        return (
+            <div className="drag-container">
+                <h2 className="head">To Do List Drag & Drop</h2>
+                <div className="inProgress"
+                     onDragOver={(event)=>this.onDragOver(event)}
+                     onDrop={(event)=>{this.onDrop(event, "inProgress")}}>
+                    <span className="group-header">In Progress</span>
+                    {tasks.inProgress}
+                </div>
+                <div className="droppable"
+                     onDragOver={(event)=>this.onDragOver(event)}
+                     onDrop={(event)=>this.onDrop(event, "Done")}>
+                    <span className="group-header">Done</span>
+                    {tasks.Done}
+                </div>
+            </div>
+        );
     }
 }
 export default Admin;
