30f48aea9cbf3f174ea2e51dd75271e611642aab
RatonII <mariuss2007@gmail.com>
2019-05-06 17:06:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Admin from './Admin';
-import VideoPage from './App';
 import * as serviceWorker from './serviceWorker';
 //pt butonul din header--pt culoare am instalat mdbreact
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-
+import Videos from './Videos';
 import {Switch, Route} from 'react-router';
 
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
-    <App/>
-), document.getElementById('root'));
+        <BrowserRouter>
+            <Switch>
+                <div>
+                    <Route path="/" exact strict component={App} />
+                    <Route path="/admin" exact strict component={Admin} />
+                    <Route path={"/videos/" + sessionStorage.getItem('id')} exact strict component={Videos} />
+                </div>
+            </Switch>
+        </BrowserRouter >
+    ), document.getElementById('root'));
+
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
